0c62920a6058ae666075ca5fdb6d1993ab82f98f
axelcorrea <73144232+axelcorrea@users.noreply.github.com>
2021-09-01 23:38:09 -0300

    Add files via upload

---
+++ clase-uno/src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+
+function App() {
+  return (
+    <div>
+      <header className="alet alert-info">
+       <h1> Hola Mundo React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
